7c3b7295f4f994cf85e43533a6afecc13d2bb03d
R1ckNash <Carapaceeee@rambler.ru>
2020-07-16 18:18:11 +0300

    bg-1 added admin, base style for home page

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Beer, Producer, Style, Category
+
+
+@admin.register(Beer)
+class BeerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'volume', 'producer', 'score')
+
+
+class ProducerAdmin(admin.ModelAdmin):
+    list_display = ('country', 'city', 'brewer')
+
+
+admin.site.register(Producer, ProducerAdmin)
+admin.site.register(Style)
+admin.site.register(Category)
