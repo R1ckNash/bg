b16bd84b663a6d11d53200807cbd6f205a3a4181
R1ckNash <Carapaceeee@rambler.ru>
2020-07-19 16:18:02 +0300

    bg-5 added registration and login

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from . import views
 from django.conf.urls import url
 
-
+app_name = 'catalog'
 urlpatterns = [
     #path('', views.index, name='index'),
     url(r'^$', views.index, name='index'),
@@ -10,4 +11,5 @@ urlpatterns = [
     url(r'^beer/(?P<pk>\d+)$', views.BeerDetailView.as_view(), name='beer-detail'),
     url(r'^producers/$', views.ProducerListView.as_view(), name='producers'),
     url(r'^producer/(?P<pk>\d+)$', views.ProducerDetailView.as_view(), name='producer-detail'),
+    path('accounts/registration/', views.registration, name = 'registration'),
 ]
